af566ca1dd137d4e24f0d7123bbebfb6f7c1d0cb
KamieniarzJakub <jakub.kamieniarz@student.put.poznan.pl>
2025-11-02 21:27:00 +0100

    BibTeX configuration

--- script.ps1
+++ script.ps1
@@ -8,4 +8,16 @@ if (!(Test-Path -Path $outDir)) {
     New-Item -ItemType Directory -Path $outDir | Out-Null
 }
 
-pdflatex -output-directory=$outDir -jobname=$output $filename
+Write-Host "[1/4] Kompilacja LaTeX..."
+pdflatex -interaction=nonstopmode -output-directory=$outDir -jobname=$output $filename | Out-Null
+
+Write-Host "[2/4] Uruchamianie BibTeX..."
+bibtex "$outDir\$output" | Out-Null
+
+Write-Host "[3/4] Druga kompilacja LaTeX..."
+pdflatex -interaction=nonstopmode -output-directory=$outDir -jobname=$output $filename | Out-Null
+
+Write-Host "[4/4] Ostatnia kompilacja LaTeX..."
+pdflatex -interaction=nonstopmode -output-directory=$outDir -jobname=$output $filename | Out-Null
+
+Write-Host "Kompilacja zakończona. Wynik: $outDir\$output.pdf"
